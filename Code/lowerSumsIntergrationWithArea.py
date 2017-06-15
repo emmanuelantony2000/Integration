b996764795de55e03284d8b75fcf58606bda1693
Emmanuel Antony <emmanuelantony2000@gmail.com>
2017-06-15 21:41:15 +0530

    Add files via upload
    Fixed the area calculation

--- Code/lowerSumsIntergrationWithArea.py
+++ Code/lowerSumsIntergrationWithArea.py
@@ -1,6 +1,6 @@
-import time
 import matplotlib.pyplot as plt
 from matplotlib import style
+from math import sin, cos, tan, pi
 
 style.use('ggplot')
 plt.ion()
@@ -16,25 +16,29 @@ def plotting(func, fr, miniSteps, colour):
 
     plt.plot(X, y, color=colour)
 
-funcRange = [1, 2.71828]
+funcRange = [-5.2, 5.2]
 interval = funcRange[1] - funcRange[0]
 steps = 2
 area = 0.0
-colour = 'red'
-iteration = 10
-function = '1/x'
+colour = 'yellow'
+colourRectangle = 'blue'
+iteration = 8
+function = '(x**2 - 1)*(x**2 - 4)*(x**2 - 9)*(x**2 - 16)*(x**2 - 25)'
 
 for i in range(iteration):
     area = 0.0
     plt.clf()
-    plotting(function, funcRange, 0.001, 'blue')
+    plotting(function, funcRange, 0.001, colour)
     x = funcRange[0]
     jump = interval/steps
     for j in range(steps):
-        plt.plot([x, x], [0, eval(function)], color='red')
-        plt.plot([x, (x+jump)], [eval(function), eval(function)], color='red')
-        plt.plot([(x+jump), (x+jump)], [eval(function), 0], color='red')
-        area += (jump * eval(function))
+        plt.plot([x, x], [0, eval(function)], color=colourRectangle)
+        plt.plot([x, (x+jump)], [eval(function), eval(function)],
+                 color=colourRectangle)
+        plt.plot([(x+jump), (x+jump)], [eval(function), 0],
+                 color=colourRectangle)
+        area += (jump * eval(function)) if (jump * eval(function)) >= 0 \
+                else -(jump * eval(function))
         x += jump
 
     steps *= 2
@@ -44,5 +48,4 @@ for i in range(iteration):
     plt.text((x+(interval/32)), (eval(function)+(interval/32)), ('Area = '+str(area)))
     plt.show()
     plt.pause(0.5)
-    #time.sleep(2)
     
