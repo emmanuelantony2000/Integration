6b3a08cccc6b1bcb03a2daeb9edfc8d9478474c2
Emmanuel Antony <emmanuelantony2000@gmail.com>
2017-06-20 22:59:38 +0530

    Add files via upload
    Made the program run faster and efficient.

--- Code/upperSumsIntegration.py
+++ Code/upperSumsIntegration.py
@@ -1,43 +1,43 @@
-import time
 import matplotlib.pyplot as plt
 from matplotlib import style
 
 style.use('ggplot')
 plt.ion()
 
-def plotting(func, fr, miniSteps, colour):
-    X = []
-    y = []
-    x = fr[0]
-    while x <= fr[1]:
-        X.append(x)
-        y.append(eval(func))
-        x += miniSteps
-
-    plt.plot(X, y, color=colour)
-
 funcRange = [-4, 4]
 interval = funcRange[1] - funcRange[0]
 steps = 2
+miniSteps = 0.001
 colour = 'red'
-iteration = 8
+graphColour = 'blue'
+iteration = 10
 function = 'x**3 - 6*x'
+
+X = []
+y = []
+x = funcRange[0]
+while x <= funcRange[1]:
+    X.append(x)
+    y.append(eval(function))
+    x += miniSteps
 
 for i in range(iteration):
     plt.clf()
-    plotting(function, funcRange, 0.001, 'blue')
+    plt.plot(X, y, color=graphColour)
     x = funcRange[0]
     jump = interval/steps
     for j in range(steps):
         x += jump
-        plt.plot([x, x], [0, eval(function)], color='red')
-        plt.plot([x, (x-jump)], [eval(function), eval(function)], color='red')
-        plt.plot([(x-jump), (x-jump)], [eval(function), 0], color='red')
+        plt.plot([x, x], [0, eval(function)], color=colour)
+        plt.plot([x, (x-jump)], [eval(function), eval(function)],
+                 color=colour)
+        plt.plot([(x-jump), (x-jump)], [eval(function), 0], color=colour)
 
     steps *= 2
     plt.xlabel('x')
     plt.ylabel('f(x)')
+    x = funcRange[1]
+    plt.text(1.1*funcRange[1], eval(function), 'i='+str(i+1))
     plt.show()
     plt.pause(0.5)
-    #time.sleep(2)
     
